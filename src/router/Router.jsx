30e632edcea6bea4ace2133e8fe279f3473d7180
joacocaturelli <joacocatu@gmail.com>
2026-07-12 23:19:54 +0100

    Feat: Añadido el Router con todas las Pages enrutadas dentro del Layout y la NotFound

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductsPage from "../pages/ProductsPage/ProductsPage";
+import ProductDetailPage from "../pages/ProductsDetailPage/ProductDetailPage";
+import Layout from "../components/Layout/Layout";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import HomePage from "../pages/HomePage/HomePage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/productos",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/productos/:productId",
+        element: <ProductDetailPage />,
+      },
+    ],
+  },
+])
+
+
 function Router() {
-  return <HomePage />
+  return <RouterProvider router={router} />
 }
 
 export default Router
