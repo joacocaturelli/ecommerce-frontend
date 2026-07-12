4245ff64dc4c7b4b3b21a399f552d941df61e64a
joacocaturelli <joacocatu@gmail.com>
2026-07-12 23:15:48 +0100

    Feat: Actualizada la Home y añadidos sus estilos

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,20 @@
-import Header from "../../components/Header/Header"
+import { Link } from "react-router-dom"
+import styles from './HomePage.module.css'
 
-function HomePage() {
+function HomePage () {
   return (
-    <main>
-      <Header />
-      <section>
-        <p>Soy el contenido principal de HomePage</p>
-      </section>
+    <main className={styles.page}>
+      <article className={styles.container}>
+        <div className={styles.infoContainer}>
+          <p className={styles.selection}>EDITORIAL SELECTION</p>
+          <h1 className={styles.title}>Prendas, calzado y accesorios con una línea más sobria y atemporal.</h1>
+          <p className={styles.text}> Descubre una selección pensada para vestir el día a día con equilibrio, comodidad y una estética más cuidada.</p>
+        </div>
+        <div className={styles.linkContainer}>
+          <p className={styles.text2}>Colección de temporada con envíos rápidos, cambios sencillos y una presentación más limpia del producto.</p>
+          <Link className="button" to='/productos'>Ver colección</Link>
+        </div>
+      </article>
     </main>
   )
 }
