afa59ff823231d4a4e3260e85030ab8dda7b0f23
joacocaturelli <joacocatu@gmail.com>
2026-07-18 20:18:03 +0100

    Feat: Actualizada ProductsPage para que reciba los productos desde el Back

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,16 +1,34 @@
 import styles from "./ProductsPage.module.css"
-import { mockProducts } from '../../data/mockProducts.js'
 import ProductGrid from "../../components/ProductGrid/ProductGrid.jsx"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getProducts } from "../../api/products.js"
+import StatusMessage from "../../components/StatusMessage/StatusMessage.jsx"
 
 function ProductsPage() {
 
+  const [products, setProducts] = useState([]) // Estado para el array de productos
+  const [loading, setLoading] = useState(false) // Estado para Loading
+  const [error, setError] = useState(null) // Estado para el manejo de errores
   const [search, setSearch] = useState('')
 
-  const visibleProducts = mockProducts.filter(
-    (product) => 
-      product.name.toLowerCase().includes(search.toLowerCase()) || 
-      product.tag.toLowerCase().includes(search.toLowerCase())
+  useEffect(() => {
+    async function showProducts() {
+      try {
+        setLoading(true)
+
+        const data = await getProducts() // Traemos los productos
+        setProducts(data) // Los guardamos en products
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setLoading(false) // Indicamos en el estado de loading que no esta cargando
+      }
+    }
+    showProducts()
+  }, []) // Solo se ejecuta useEffect al montar ProductsPage (solo una vez)
+
+  const visibleProducts = products.filter(
+    (product) => product.name.toLowerCase().includes(search.toLowerCase())
   )
 
   return (
@@ -28,7 +46,23 @@ function ProductsPage() {
           onChange={(evento) => setSearch(evento.target.value)}
         />
       </section>
-      <ProductGrid products={visibleProducts}/>
+
+      {loading && (
+        <StatusMessage 
+          title='Cargando productos...' 
+          description='Esperando respuesta del backend...' 
+        />
+      )}
+
+      {error && (
+        <StatusMessage 
+          title='Ha ocurrido un error'
+          description={error}
+          variant="error"
+        />
+      )}
+
+      {!loading && !error && <ProductGrid products={visibleProducts}/>}
     </main>
   )
 }
