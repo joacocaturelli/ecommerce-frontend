f30cb4f57da443e437d9ad6099ef95704d985e95
joacocaturelli <joacocatu@gmail.com>
2026-07-18 20:16:42 +0100

    Feat: Implementado StatusMessage en ProductGrid

--- src/components/ProductGrid/ProductGrid.jsx
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -1,7 +1,20 @@
 import ProductCard from "../ProductCard/ProductCard";
 import styles from './ProductGrid.module.css'
+import StatusMessage from "../StatusMessage/StatusMessage";
 
 function ProductGrid({products}) {
+
+  if (products.length === 0) {
+    return (
+      <section className={styles.grid}>
+        <StatusMessage 
+          title='No se ha encontrado ningun producto con ese nombre...'
+          description='Prueba buscando algun otro producto'
+        />
+      </section>
+    )
+  }
+  
   return (
     <section className={styles.grid}>
       {products.map((p) => (
