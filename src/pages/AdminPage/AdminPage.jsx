98fd6a58a547abc60f2f051fa2383e4b4c46440d
joacocaturelli <joacocatu@gmail.com>
2026-07-26 21:44:36 +0100

    Feat: Añadida la ruta privada AdminPage con sus estilos

---
+++ src/pages/AdminPage/AdminPage.jsx
@@ -0,0 +1,15 @@
+import styles from './AdminPage.module.css'
+
+function AdminPage () {
+  return (
+    <main className="page">
+      <section className="container">
+        <div className={styles.container}>
+          <h2>Bienvenido Admin</h2>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default AdminPage
