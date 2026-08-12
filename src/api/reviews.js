0f11acfc9a2b8aa24f53e68c37d5ac3648bca2b2
joacocaturelli <joacocatu@gmail.com>
2026-08-12 22:27:01 +0100

    Feat: Creadas las peticiones de axios para las reviews

--- src/api/reviews.js
+++ src/api/reviews.js
@@ -4,3 +4,29 @@ export async function getReviewsByProductId(productId) {
   const response = await apiClient.get(`/products/${productId}/reviews`);
   return response.data.data;
 }
+
+export async function getReviewsByUser() {
+  const response = await apiClient.get(`/reviews`);
+  return response.data.data;
+}
+
+export async function createReview(productId, rating, comment) {
+  const response = await apiClient.post(`/products/${productId}/reviews`, {
+    rating,
+    comment,
+  });
+  return response.data.data;
+}
+
+export async function updateReview(productId, rating, comment) {
+  const response = await apiClient.put(`/reviews/${productId}`, {
+    rating,
+    comment,
+  });
+  return response.data.data;
+}
+
+export async function deleteReview(productId) {
+  const response = await apiClient.delete(`/reviews/${productId}`);
+  return response.data.data;
+}
