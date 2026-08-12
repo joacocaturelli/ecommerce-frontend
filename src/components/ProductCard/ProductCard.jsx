bf9962a641270896547116a084176a8b637297c5
joacocaturelli <joacocatu@gmail.com>
2026-08-12 22:20:53 +0100

    Feat: Modificado el componente para recibir y renderizar productos de la coleccion, el cart y la wishlist

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,29 +1,84 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addProductThunk, removeProductThunk } from '../../store/features/cartSlice'
 import Button from '../Button/Button'
 import styles from './ProductCard.module.css'
+import { addToWishlistThunk, removeFromWishlistThunk } from '../../store/features/wishlistSlice'
 
-function ProductCard({product}) {
+function ProductCard({product, quantity}) {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { user } = useSelector((state) => state.auth)
+  const { items } = useSelector((state) => state.wishlist)
+
+  const productId = product.id
+
+  const isInWish = items.some((item) => item.id === productId)
+  
+  function handleAddToCart() {
+    dispatch(addProductThunk({productId, quantity: 1}))
+  }
+  
+  function handleRemoveToCart() {
+    dispatch(removeProductThunk(productId))
+  }
+
+  function handleAddToWish() {
+    dispatch(addToWishlistThunk(productId))
+  }
+
+  function handleRemoveFromWish() {
+    dispatch(removeFromWishlistThunk(productId))
+  }
+
+  function handleNavigate() {
+    navigate('/login')
+  }
+
   return (
     <article className={styles.card}>
       <div className={styles.productHeader}>
         <p className={product.stock > 0 ? styles.stockOk : styles.stockNo}>
           {product.stock > 0 ? 'En Stock' : 'Sin Stock'}
         </p>
+
+        <button 
+          className={styles.toggleFav} 
+          onClick={
+            !user
+              ? handleNavigate
+              : isInWish
+                ? handleRemoveFromWish
+                : handleAddToWish
+          }
+        >
+          {isInWish ? '♥' : '♡'}
+        </button>
       </div>
-    
-      <h2 className={styles.title}>{product.name}</h2>
-      <p className={styles.description}>{product.description}</p>
+
+      <Link to={`/productos/${product.id}`}>
+        <h2 className={styles.title}>{product.name}</h2>
+        <p className={styles.description}>{product.description}</p>
+      </Link>
 
       <div className={styles.productFooter}>
         <h3 className={styles.price}>{product.price}€</h3>
 
-        <Button size='small'>
-          <Link to={`/productos/${product.id}`}>
-            Ver producto
-          </Link>
-        </Button>
-      </div>
+        {quantity ? (
+          <>
+            <p>Cantidad: {quantity}</p>
 
+            <Button onClick={handleRemoveToCart} size='small'>
+              Eliminar del carrito
+            </Button>
+          </>
+        ) : (
+          <Button onClick={user? handleAddToCart : handleNavigate} size='small'>
+            Añadir al carrito
+          </Button>
+        )}
+      </div>
     </article>
   )
 }
