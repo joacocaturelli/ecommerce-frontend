f8479be8cf9811b3e13ab0dcd75993fc1650fa6f
joacocaturelli <joacocatu@gmail.com>
2026-08-12 22:25:44 +0100

    Feat: Añadidos botones add y remove to cart y wishlist

--- src/pages/ProductsDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductsDetailPage/ProductDetailPage.jsx
@@ -1,28 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useProduct } from "../../hooks/useProduct";
-import { addToCart } from '../../store/features/cartSlice.js';
-import { useDispatch } from "react-redux";
+import { addProductThunk } from '../../store/features/cartSlice.js';
+import { addToWishlistThunk, removeFromWishlistThunk } from '../../store/features/wishlistSlice'
 import Button from "../../components/Button/Button";
 import ReviewList from "../../components/ReviewList/ReviewList";
 import StatusMessage from "../../components/StatusMessage/StatusMessage";
 import styles from './ProductDetailPage.module.css'
 
 function ProductDetailPage() {
+  const dispatch = useDispatch()
   const { productId } = useParams()
   const { product, loading, error } = useProduct(productId)
-  const dispatch = useDispatch()
+  const [ quantity, setQuantity ] = useState(1)
 
-  const [count, setCount] = useState(1)
+  const { user } = useSelector((state) => state.auth)
+  const { items } = useSelector((state) => state.wishlist)
+  const isInWish = items.some((item) => item.id === productId)
 
   const restar = () => {
-    if(count > 1) {
-      setCount(count - 1)
+    if(quantity > 1) {
+      setQuantity(quantity - 1)
     }
   }
 
   function handleAddToCart() {
-    dispatch(addToCart())
+    dispatch(addProductThunk({productId, quantity}))
+  }
+
+  function handleAddToWish() {
+    dispatch(addToWishlistThunk(productId))
+  }
+
+  function handleRemoveFromWish() {
+    dispatch(removeFromWishlistThunk(productId))
+  }
+
+  function handleNavigate() {
+    navigate('/login')
   }
 
   if (loading) {
@@ -75,19 +91,34 @@ function ProductDetailPage() {
               {product.stock > 0 ? 'En Stock' : 'Sin Stock'}
             </p>
           </div>
+
           <h2 className={styles.title}>{product.name}</h2>
           <p className={styles.description}>{product.description}</p>
           <p className={styles.price}>{product.price}€</p>
         </div>
+
         <div className={styles.infoCart}>
           <div className={styles.counter}>
             <button className={styles.btnCart} onClick={restar}>-</button>
-            <span className={styles.quantity}>{count}</span>
-            <button className={styles.btnCart} onClick={() => setCount(count + 1)}>+</button>
+            <span className={styles.quantity}>{quantity}</span>
+            <button className={styles.btnCart} onClick={() => setQuantity(quantity + 1)}>+</button>
           </div>
+
           <div className={styles.btnCartContainer}>
             <Button onClick={handleAddToCart}>Añadir al carrito </Button>
-            <Button>Añadir a la wishlist</Button>
+            <Button onClick={
+              !user
+                ? handleNavigate
+                : isInWish 
+                  ? handleRemoveFromWish
+                  : handleAddToWish
+              }
+            >
+              {isInWish
+                ? 'Eliminar de la wishlist'
+                : 'Añadir a la wishlist'
+              }
+            </Button>
           </div>
         </div>
       </div>
