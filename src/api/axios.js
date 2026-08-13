4c65553009e33b342cead42839f7aea9ed5522d7
joacocaturelli <joacocatu@gmail.com>
2026-08-13 23:07:05 +0100

    Chore: Eliminado interceptor porque ya no se utiliza el token en cabecera

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,17 +6,4 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
-// El interceptor añade el token automaticamente a las cabeceras
-// en cada llamada a la API para no tener que hacerlo manualmente
-apiClient.interceptors.request.use((config) => {
-  const token = store.getState().auth.token;
-  // store.getState() lee el estado actual de Redux al momento de la peticion
-
-  if (token) {
-    // config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
 export default apiClient;
